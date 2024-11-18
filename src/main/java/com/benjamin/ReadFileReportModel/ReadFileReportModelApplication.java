b6be85ad64032b8fb8e06e8174c2902e022c02c8
LAPTOP-4UHPN8A3\Benjamin Wong <benjamin_wong98@hotmail.com>
2024-11-18 22:18:19 +0800

    Model Project Launch

---
+++ src/main/java/com/benjamin/ReadFileReportModel/ReadFileReportModelApplication.java
@@ -0,0 +1,13 @@
+package com.benjamin.ReadFileReportModel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadFileReportModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadFileReportModelApplication.class, args);
+	}
+
+}
